693540c0d5da651ee4cb9e6f420b4bbf5ca88538
Anita <sahooanitarani125@gmail.com>
2021-11-09 00:19:03 +0530

    hi

---
+++ Basic Programs/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic_Programs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("please enter your name");
+            string name = Console.ReadLine();//rani
+                                             //0123
+            int len = name.Length;
+            for(int i=name.Length-1;i>=0;i--)
+            {
+                Console.Write(name[i]);
+            }
+            Console.ReadLine();
+           
+        }
+        
+    }
+}
